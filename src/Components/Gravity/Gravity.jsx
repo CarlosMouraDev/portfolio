6a969b35dba9a50df102046c951c40360ef4cbbf
Carlos Moura <carlos@email.com>
2024-09-17 11:10:47 -0300

    melhora o botao de download do curriculo

--- src/Components/Gravity/Gravity.jsx
+++ src/Components/Gravity/Gravity.jsx
@@ -3,6 +3,7 @@ import me from "/assets/me.jpeg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import DownloadIcon from "/assets/downloadIcon.png"
 
 const Gravity = () => {
   useEffect(() => {
@@ -20,23 +21,40 @@ const Gravity = () => {
       <div id={styles.earth}></div>
       <div className={styles.container}>
         <div className={styles.content1}>
-          <img data-aos-duration="1000" data-aos="zoom-in" className={styles.image} src={me} alt="Minha foto" />
-          <p data-aos-duration="1000" data-aos="zoom-in" className={styles.name}>Carlos Eduardo</p>
+          <img
+            
+            data-aos-duration="1000"
+            data-aos="zoom-in"
+            className={styles.image}
+            src={me}
+            alt="Minha foto"
+          />
+          <p
+            data-aos-duration="1000"
+            data-aos="zoom-in"
+            className={styles.name}
+          >
+            Carlos Eduardo
+          </p>
         </div>
         <div className={styles.content2}>
-          <h1 data-aos="zoom-out-down" data-aos-duration="1000">Programador Full-Stack</h1>
+          <h1 data-aos="zoom-out-down" data-aos-duration="1000">
+            Programador Full-Stack
+          </h1>
           <p data-aos="zoom-out-left" data-aos-duration="1000">
             Estou buscando minha primeira vaga de trabalho como desenvolvedor
             full-stack para adentrar no mercado e conseguir experiências tanto
             na programação quanto no trabalho em equipe.
           </p>
-          <div className={styles.downloadContainer}><a
+          <a
             className={styles.cv}
             href="/assets/curriculo.pdf"
             download="carlos-curriculo.pdf"
+            data-aos="zoom-in"
+            data-aos-duration="1000"
           >
-            Download CV
-          </a></div>
+          CV <img className={styles.download} src={DownloadIcon} alt="Icone download" />
+          </a>
         </div>
       </div>
     </div>
